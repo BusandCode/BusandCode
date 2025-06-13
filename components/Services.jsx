d8d3b5297fc8156ef1523da628494f4c8b04044f
Adetokunbo Andrew <Adetokunboandrew2@gmail.com>
2025-06-13 23:24:56 +0100

    service

--- components/Services.jsx
+++ components/Services.jsx
@@ -12,24 +12,26 @@ const Services = () => {
             <p className='text-[20px] text-gray-400'>I am a frontend developer from California, USA with 10 years of experience in multiple 
             companies like Microsoft, Tesla and Apple.</p>
         </div>
-        <div className='grid grid-cols-1 lg:grid-cols-4 gap-5 mt-10 px-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10 px-5 place-items-center'>
             <article className='max-w-[335.49px] max-h-[329px] cursor-pointer 
             rounded-[8px] border-[2px] border-[#EFEFEF] 
-            flex flex-col gap-5 items-start p-5 '>
+            flex flex-col gap-5 items-start p-5  hover:shadow-lg hover:shadow-orange-500/20 hover:border-orange-400 hover:bg-gray-800/10 transition-all duration-300 '>
                 <AiOutlineBehance className='text-[white] text-[20px] p-3 bg-[#FF6E00] w-[60px] h-[60px] rounded-[10px]' />
                 <small className='font-medium text-[26px] text-white'>Web development</small>
                 <small className='font-normal text-[16px] text-gray-400'>Creating responsive and modern websites using the latest technologies like React, Next.js, and Tailwind CSS to deliver exceptional user experiences.</small>
             </article>
             <article className='max-w-[335.49px] max-h-[329px]  cursor-pointer rounded-[8px] border-[2px] border-[#EFEFEF] 
-            flex flex-col gap-5 items-start p-5 '>
+            flex flex-col gap-5 items-start p-5 
+             hover:shadow-lg hover:shadow-orange-500/20 hover:border-orange-400 hover:bg-gray-800/10 transition-all duration-300 
+            '>
                 {/* <img src="/mobile-icon.png" alt="" width={47.62} height={47.62}/> */}
-                <AiFillAndroid className='text-[white] text-[20px] p-3 bg-[#FF6E00] w-[60px] h-[60px] rounded-[10px]' />
+                <AiFillAndroid className='text-[white] text-[20px] p-3 bg-[#FF6E00] w-[60px] h-[60px] rounded-[10px] ' />
                 <small className='font-medium text-[26px] text-white'>Mobile app</small>
                 <small className='font-normal text-[16px] text-gray-400'>
                 Building cross-platform mobile applications using React Native to bring your ideas to life on iOS and Android devices.</small>
             </article>
             <article className='max-w-[335.49px] cursor-pointer max-h-[329px] rounded-[8px] border-[2px] border-[#EFEFEF] 
-            flex flex-col gap-5 items-start p-5 '>
+            flex flex-col gap-5 items-start p-5  hover:shadow-lg hover:shadow-orange-500/20 hover:border-orange-400 hover:bg-gray-800/10 transition-all duration-300 '>
                 <SiAdobephotoshop className='text-[white] text-[20px] p-3 bg-[#FF6E00] w-[60px] h-[60px] rounded-[10px]'/>
                 {/* <img src="/graphics-icon.png" alt="" width={47.62} height={47.62}/> */}
                 <small className='font-medium text-[26px] text-white'>Graphics design</small>
